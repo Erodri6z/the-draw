16992a916c44675353a2422f7be555f5ab3e1281
Erodri6z <erikrodriguez2417@gmail.com>
2025-05-15 21:17:55 -0500

    stub up a form

---
+++ src/components/questions/questions.jsx
@@ -0,0 +1,25 @@
+const questions = () => {
+  return (
+    <>
+    <form>
+    <div>
+      <label htmlFor="dob">Date of Birth</label>
+      <input type="date"/>
+      </div>
+      <div>
+      <label>Relationship Status</label>
+      <input type="selection"/>
+      </div>
+      <div>
+      <label>How Do You Feel</label>
+      <input type="text"/>
+      </div>
+      <div>
+      <label>Personal Goal</label>
+      <input type="text"/>
+      </div>
+    </form>
+
+    </>
+  )
+}
