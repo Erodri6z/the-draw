aec306fce826aaf184a8551adfb986cbd48959f8
Erodri6z <erikrodriguez2417@gmail.com>
2025-05-20 22:34:06 -0500

    add a way to get the horosocope instead of the dob

--- src/components/questions/questions.jsx
+++ src/components/questions/questions.jsx
@@ -12,24 +12,34 @@ const Questions = () => {
 
   const getHoroscope = (dateString) => {
     const date = new Date(dateString)
-    console.log(date)
     const month = (date.getMonth() + 1).toString().padStart(2, "0")
-    const day = date.getDate().toString().padStart(2, "0")
+    const day = (date.getDate() + 1).toString().padStart(2, "0")
     const formatted = `${month}-${day}`
+    console.log(formatted)
 
     return zodiacSigns.find(({ start, end }) => {
       return formatted >= start && formatted <= end
     })?.sign || "Unknown"
   }
 
+  const handleDob = (e) => {
+    setSpiritualData({
+      ...spiritualData,
+      horoscope : getHoroscope(e.target.value)
+    })
+
+  }
 
 
   return (
     <>
+    <div>
+      <h2>{spiritualData.horoscope}</h2>
+    </div>
     <form>
     <div>
       <label>Date of Birth</label>
-      <input type="date" id="dob"/>
+      <input type="date" id="dob" name="dob" onChange={handleDob}/>
       </div>
       <div>
       <label>Relationship Status</label>
