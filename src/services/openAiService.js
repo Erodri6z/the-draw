7e2798a2a62a209ebb88080fc0d233fad5bc26f9
Erodri6z <erikrodriguez2417@gmail.com>
2025-06-25 19:12:14 -0500

    add the service function to allow the prompt top be generated

--- src/services/openAiService.js
+++ src/services/openAiService.js
@@ -1,3 +1,17 @@
+import * as tokenService from './tokenService'
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/openai/reading`
+
 async function getReading(prompt) {
-  // holding off on building this until profiles auth is set up
+  const res = await fetch (BASE_URL, {
+    method: "POST",
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({ prompt })
+  })
+
+  const data = await response.json()
+  return data.response
 }
