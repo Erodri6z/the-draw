23b850efa020fdc58acaeeeb3eaaa84676a88ec5
Erodri6z <erikrodriguez2417@gmail.com>
2025-06-28 14:10:10 -0500

    add some css for the cards

--- src/components/drawnCards/drawCard.jsx
+++ src/components/drawnCards/drawCard.jsx
@@ -1,3 +1,5 @@
+import './drawCard.css'
+
 const DrawnCard = ({slug}) => {
   const BASE_URL = import.meta.env.VITE_BACK_END_SERVER_URL
 
@@ -7,12 +9,12 @@ const DrawnCard = ({slug}) => {
   {
     slug.name.includes('(Upsidedown)')?
     <div key={slug}>
-      <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card"  style={{ width: "100px", height: "200px", transform: "rotate(180deg)" }} />
+      <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card" className="upsidedown-card card" style={{ width: "150px", height: "250px", transform: "rotate(180deg)" }} />
     </div>
 
     :
     <div key={slug}>
-      <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card"  style={{ width: "100px", height: "200px" }} />
+      <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card" className="upright-card card" style={{ width: "150px", height: "250px" }} />
     </div>
 
   }
