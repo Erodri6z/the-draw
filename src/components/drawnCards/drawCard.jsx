f67ce8ecab2ea62139b3c07f89708fee57d1de0b
Erodri6z <erikrodriguez2417@gmail.com>
2025-06-29 12:52:04 -0500

    add some animations and css for cards

--- src/components/drawnCards/drawCard.jsx
+++ src/components/drawnCards/drawCard.jsx
@@ -1,23 +1,37 @@
 import './drawCard.css'
+import { useState } from 'react';
 
 const DrawnCard = ({slug}) => {
+  const [flipped, setFlipped] = useState(false);
+
   const BASE_URL = import.meta.env.VITE_BACK_END_SERVER_URL
 
 
   return (
   <>
-  {
-    slug.name.includes('(Upsidedown)')?
-    <div key={slug}>
-      <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card" className="upsidedown-card card" />
-    </div>
+    <div
+      className={`flip-card ${flipped ? 'flipped' : ''}`}
+      onClick={() => setFlipped(!flipped)}
+    >
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <p>Click to Reveal</p>
+        </div>
+        <div className="flip-card-back">
+          {
+            slug.name.includes('(Upsidedown)')?
+            <div key={slug}>
+              <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card" className="upsidedown-card card" />
+            </div>
 
-    :
-    <div key={slug}>
-      <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card" className="upright-card card" />
+            :
+            <div key={slug}>
+              <img src={`${BASE_URL}/api/tarot/${slug.image}`} alt="card" className="upright-card card" />
+            </div>
+          }
+        </div>
+      </div>
     </div>
-
-  }
   </>
   )
 }   
