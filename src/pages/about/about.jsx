f70a9aefc3d3820d3b723abeaf5e356cd3d8033f
Erodri6z <erikrodriguez2417@gmail.com>
2025-07-07 20:23:13 -0500

    add some tags and basic css for the about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,15 +1,16 @@
+import './about.css'
 const About = () => {
   return (
     <>
-    <div>
+    <div className="about">
       <div className="heading">
         <h1>The Draw</h1>
       </div>
-      <div>
+      <div className='section'>
         <h3>What are Tarot cards</h3>
         <p>Tarot cards are a deck of symbolic cards used for divination, self-reflection, spiritual insight, or even creative storytelling. Though often seen as mystical or magical, they’re really a tool — like a mirror — that helps people explore their inner thoughts, questions, and possibilities.</p>
       </div>
-      <div>
+      <div className='section'>
         <h3>Explaination</h3>
         <p>When you draw three tarot cards completely at random, you're doing more than pulling symbols from a deck — you're opening a quiet space for the universe to speak directly to you.
         <br />
@@ -19,19 +20,19 @@ const About = () => {
         A three-card draw is often simple in form but profound in meaning.
         </p>
         <h4>Each Position Can Show:</h4>
-        <li>Where you've been (Past),</li>
-        <li>Where you are (Present),</li>
-        <li>And what may come (Future).</li>
+        <li className="list">Where you've been (Past),</li>
+        <li className="list">Where you are (Present),</li>
+        <li className="list">And what may come (Future).</li>
         <h4>Or it may show:</h4>
-        <li>Your Mind</li>
-        <li>Your Heart</li>
-        <li>Your Path Forward</li>
+        <li className="list">Your Mind</li>
+        <li className="list">Your Heart</li>
+        <li className="list">Your Path Forward</li>
       </div>
-      <div>
+      <div className='section'>
         <p>There's no single rule, just an invitation to listen, reflect and feel</p>
         <p>You're not summoning magic from outside yourself, You're revealing what is already inside you, just waiting to be seen. So when you are getting your reading, take a breath and let the cards speak.Trust that what ever came up, was always ment to.</p>
       </div>
-      <div>
+      <div className='section'>
         <h4>Best Practice:</h4>
         <p>Here with The Draw team, while we encourage users to try to get a reading when ever they feel they need a moment to reflect, we encourage moderation due to the fact that trying to force anwsers you want to hear will only confuse and close the line of communication between you and the universe.</p>
       </div>
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -5,10 +5,9 @@ const Landing = ({ user }) => {
     <>
     {user?
     <div>
-      <h1>Welcome to The Draw</h1>
+      <h1>Welcome to The Draw (Beta)</h1>
       <img src="/theDrawEdit.png" alt="The Draw" className="main-logo"/>
-      <p>Depending on how you feel is how the reading will work.</p>
-      <h6>Feel Good Quote here</h6>
+      <p>When you draw three cards at random, you give the universe a chance to speak — unfiltered, untampered, and just for you.</p>
       <p>Get Started? Click Below</p>
       <a href="/reading">
         <button>Get Started</button>
@@ -16,8 +15,9 @@ const Landing = ({ user }) => {
     </div>
     :
     <div>
-      <h2> Welcome to The Draw</h2>
+      <h2> Welcome to The Draw (Beta)</h2>
       <img src="/theDrawEdit.png" alt="The Draw" className="main-logo"/>
+      <p>When you draw three cards at random, you give the universe a chance to speak — unfiltered, untampered, and just for you.</p>
       <h4>Please Sign In To Continue</h4>
       <div className='login-or-signup'>
         <a href="/Login" className='login-a'>
