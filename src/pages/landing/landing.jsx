cf1ca370842d12e884e06062703a5e1f097a8595
Erodri6z <erikrodriguez2417@gmail.com>
2025-07-08 20:07:58 -0500

    add some animations for the landing

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -14,26 +14,26 @@ const Landing = ({ user }) => {
       <div className="landing-wrapper">
         {user ? (
           <div>
-            <h1>Welcome to The Draw (Beta)</h1>
+            <h2 className='app-name'>Welcome to The Draw (Beta)</h2>
             <img src="/theDrawEdit.png" alt="The Draw" className="main-logo" />
-            <p>
+            <p className='fade'>
               When you draw three cards at random, you give the universe a
               chance to speak — unfiltered, untampered, and just for you.
             </p>
-            <p>Get Started? Click Below</p>
+            <p className='fade'>Get Started? Click Below</p>
             <a href="/reading">
               <button>Get Started</button>
             </a>
           </div>
         ) : (
           <div>
-            <h2>Welcome to The Draw (Beta)</h2>
+            <h2 className='app-name'>Welcome to The Draw (Beta)</h2>
             <img src="/theDrawEdit.png" alt="The Draw" className="main-logo" />
-            <p>
+            <p className='fade'>
               When you draw three cards at random, you give the universe a
               chance to speak — unfiltered, untampered, and just for you.
             </p>
-            <h4>Please Sign In To Continue</h4>
+            <h4 className='fade'>Please Sign In To Continue</h4>
             <div className="login-or-signup">
               <a href="/Login" className="login-a">
                 <button>Login</button>
