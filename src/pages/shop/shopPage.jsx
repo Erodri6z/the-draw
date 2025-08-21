d3f3426a3a74e7cba6721e68a9f355d2cb8a0cae
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-21 14:15:38 -0500

    add a shop page

---
+++ src/pages/shop/shopPage.jsx
@@ -0,0 +1,14 @@
+const shopPage = () => {
+  return (
+    <>
+    <div className="shop">
+      <h2>These Are Our Amazon Picks</h2>
+      <div className="products">
+
+      </div>
+    </div>
+    </>
+  )
+}  
+
+export default shopPage
