7f221fad0b78af15410619cd05ecfbaa641d52bc
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-21 17:01:59 -0500

    add the donation link to the support page

--- src/pages/shop/shopPage.jsx
+++ src/pages/shop/shopPage.jsx
@@ -1,16 +1,22 @@
 import affiliateDetes from "../../data/affiliateLinks"
 import ShopCard from "../../components/shopCard/shopCard"
+import './shopPage.css'
 
 const ShopPage = () => {
   return (
     <>
     <div className="shop">
-      <h2 className="title">These Are Our Amazon Picks</h2>
+      <h1>Want to Support?</h1>
+      <h2 className="title">Check Out My Amazon Picks</h2>
       <div className="products">
         {affiliateDetes.map((a) => (
           <ShopCard props={a} key={a.pickName}/>
         ))}
       </div>
+      <h3>Or</h3>
+        <a href="https://ko-fi.com/gloomyspirit" target="_blank"  rel="noopener noreferrer" className="coffee-button">
+        ☕ Buy me a coffee
+      </a>
     </div>
     </>
   )
