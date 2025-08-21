ff000c4df35a3e8b77ce1b0e1d4d972e0a968c53
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-21 15:45:20 -0500

    add some affiliate links to affiliatedetes

--- src/data/affiliateLinks.jsx
+++ src/data/affiliateLinks.jsx
@@ -1,17 +1,19 @@
 const affiliateDetes = [
   {
-    pickName: "",
-    pickImage: "",
-    pickLink: ""
+    pickName: "Tarot Card Deck",
+    pickImage: "https://m.media-amazon.com/images/I/81fRbHBUQ5L._AC_SX679_.jpg",
+    pickLink: "https://amzn.to/4lCqxjy"
   },
     {
-    pickName: "",
-    pickImage: "",
-    pickLink: ""
+    pickName: "Tarot Journal",
+    pickImage: "https://m.media-amazon.com/images/I/81uc28+6LKL._SY385_.jpg",
+    pickLink: "https://amzn.to/3HyhaDR"
   },
     {
-    pickName: "",
-    pickImage: "",
-    pickLink: ""
+    pickName: "Tarot Guide",
+    pickImage: "https://m.media-amazon.com/images/I/91-gLg9s6KL._SY425_.jpg",
+    pickLink: "https://amzn.to/462tghG"
   },
 ]
+
+export default affiliateDetes 
