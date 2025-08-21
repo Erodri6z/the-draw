7eb9c1af4ddc2531c961ac932a6c0cf09abcc603
Erodri6z <erikrodriguez2417@gmail.com>
2025-08-21 16:42:28 -0500

    style the affiliate links to be shown

--- src/components/shopCard/shopCard.jsx
+++ src/components/shopCard/shopCard.jsx
@@ -1,10 +1,11 @@
+import './shopCard.css'
 
 const ShopCard = ({props}) => {
   return(
     <>
     <div className="pickCard">
       <a href={props.pickLink}>
-        <img src={props.pickImage} alt="amongus" className="thumbnail"/>
+        <img src={props.pickImage} alt="thubnail" className="thumbnail"/>
         <div>
           <h3>{props.pickName}</h3>
         </div>
