90e30777d9a8b8226612329aa42b3703ebc0d6a2
Erodri6z <erikrodriguez2417@gmail.com>
2025-09-21 17:52:11 -0500

    add functions to the guide card component

--- src/components/guideCard/guideCard.jsx
+++ src/components/guideCard/guideCard.jsx
@@ -1,16 +1,31 @@
-const guideCard = ({ card , data}) => {
+import { useState } from "react"
+import deck from "../../data/cards"
+
+const GuideCard = () => {
+  const [options] = useState(deck)
+  const [selected, setSelected] = useState()
   return (
     <>
     <div>
-      <img src={card.image} alt="card" />
-      <div>
-        <h4>{data.name}</h4>
-        <p>{data.meaning_up}</p>
-        <p>{data.meaning_rev}</p>
-      </div>
+      <h1>Card</h1>
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        {options.map((opt, i) => (
+          <option key={i} value={opt.name}>
+            {opt.name}
+          </option>
+        ))}
+      </select>
+      {/* <div>
+        <img src={card.image} alt="card" />
+        <div>
+          <h4>{data.name}</h4>
+          <p>{data.meaning_up}</p>
+          <p>{data.meaning_rev}</p>
+        </div>
+      </div> */}
     </div>
     </>
   )
 }
 
-export default guideCard
+export default GuideCard
--- src/pages/guide/guide.jsx
+++ src/pages/guide/guide.jsx
@@ -1,22 +1,15 @@
 import { useState } from "react"
-import deck from "../../data/cards"
+import GuideCard from "../../components/guideCard/guideCard"
 
 const GuidePage = () => {
-  const [options] = useState(deck)
-  const [selected, setSelected] = useState(["","",""])
-  
-  console.log(deck)
+
 
   return (
     <>
-      <h1>This is gonna be the guide page</h1>
-      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
-        {options.map((opt, i) => (
-          <option key={i} value={opt.name}>
-            {opt.name}
-          </option>
-        ))}
-      </select>
+    <div>
+      <GuideCard />
+      
+    </div>
     </>
   )
 }
