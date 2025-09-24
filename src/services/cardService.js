98b38ebfa3260c2d38aa82d1d727c7b04483b21c
Erodri6z <erikrodriguez2417@gmail.com>
2025-09-23 20:18:27 -0500

    stub up a service function for the cards info

--- src/services/cardService.js
+++ src/services/cardService.js
@@ -2,10 +2,19 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/v1/cards/search?q=`
 
 async function getInfo(name) {
+  let search = name.replace(" ", "20%")
+  console.log(search)
   try{
-
+    const res = await fetch (BASE_URL, search)
+    const data = await res.json()
+    console.log(data)
   }catch (error) {
     console.error('Error Getting Card Info:', error.message)
     throw error
   }
+}
+
+
+export {
+  getInfo
 }
