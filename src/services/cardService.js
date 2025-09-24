778cec429c1dc2a0b00bac2d8dd175decfa29922
Erodri6z <erikrodriguez2417@gmail.com>
2025-09-23 20:13:47 -0500

    add a service file

---
+++ src/services/cardService.js
@@ -0,0 +1,11 @@
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/v1/cards/search?q=`
+
+async function getInfo(name) {
+  try{
+
+  }catch (error) {
+    console.error('Error Getting Card Info:', error.message)
+    throw error
+  }
+}
